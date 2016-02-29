05219cb490fc07f4bdb559b2b241c8e4bbbaa473
Sivaramvt <Sivaramvt@gmail.com>
2016-02-29 09:15:56 +0530

    Added node static server.

---
+++ src/js/web/dom-api.js
@@ -0,0 +1,5 @@
+alert('test');
+var paragraphs = document.getElementsByTagName("P");
+// paragraphs[0] is the first <p> element
+// paragraphs[1] is the second <p> element, etc.
+alert(paragraphs[0].nodeName);
